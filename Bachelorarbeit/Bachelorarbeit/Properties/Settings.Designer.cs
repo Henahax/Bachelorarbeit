e68b1c8503a1ae447ba2181372e3e7d9ef56de3d
Henahax <mail@henahax.de>
2017-09-29 20:36:00 +0200

    GUI Update

--- Bachelorarbeit/Bachelorarbeit/Properties/Settings.Designer.cs
+++ Bachelorarbeit/Bachelorarbeit/Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace Bachelorarbeit.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Bachelorarbei" +
-            "t.mdf;Integrated Security=True")]
-        public string BachelorarbeitConnectionString {
-            get {
-                return ((string)(this["BachelorarbeitConnectionString"]));
-            }
-        }
     }
 }
