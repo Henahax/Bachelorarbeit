238b8c5de4ad7e3bf1246697e8fa514e5b5e7c1d
Henahax <mail@henahax.de>
2017-10-07 19:27:22 +0200

    Datenbankanbindung der Kundenliste und Suche in der Kundenliste

---
+++ Bachelorarbeit/Bachelorarbeit/Datenbank.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Data.SQLite;
+
+namespace Bachelorarbeit
+{
+    class Datenbank
+    {
+        public SQLiteConnection sqliteConnection;
+
+        public Datenbank()
+        {
+            sqliteConnection = new SQLiteConnection("Data Source=datenbank.sqlite3");
+
+            if (!File.Exists("./datenbank.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("datenbank.sqlite3");
+            }
+        }
+
+        public void VerbindungOeffnen()
+        {
+            if(sqliteConnection.State != System.Data.ConnectionState.Open)
+            {
+                sqliteConnection.Open();
+            }
+        }
+
+        public void VerbindungSchliessen()
+        {
+            if(sqliteConnection.State != System.Data.ConnectionState.Closed)
+            {
+                sqliteConnection.Close();
+            }
+        }
+    }
+}
--- Bachelorarbeit/Bachelorarbeit/MainWindow.xaml.cs
+++ Bachelorarbeit/Bachelorarbeit/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Data.SQLite;
+using System.Data;
+
 namespace Bachelorarbeit
 {
     /// <summary>
@@ -23,12 +26,63 @@ namespace Bachelorarbeit
         public MainWindow()
         {
             InitializeComponent();
+            KundenlisteFuellen();
+
+            /*
+            //Datenbank Eintrag anlegen
+            Datenbank databaseObject = new Datenbank();
+            string query = "INSERT INTO Kunden (`Vorname`, `Nachname`) VALUES (@Vorname, @Nachname)";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.OpenConnection();
+            myCommand.Parameters.AddWithValue("@Vorname", "Gundula");
+            myCommand.Parameters.AddWithValue("@Nachname", "Gause");
+            var result = myCommand.ExecuteNonQuery();
+            databaseObject.CloseConnection();
+            Console.WriteLine("Zeilen hinzugefügt : {0}", result);
+            */
+
+            /*
+            //Datenbank Einträge anzeigen
+            Datenbank databaseObject = new Datenbank();
+            string query = "SELECT * FROM Kunden";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+            databaseObject.OpenConnection();
+            SQLiteDataReader result = myCommand.ExecuteReader();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("Vorname : {0} - Nachname : {1}", result["Vorname"], result["Nachname"]);
+                }
+            }
+            databaseObject.CloseConnection();
+            */
         }
 
-        private void einstellungenOeffnen(object sender, RoutedEventArgs e)
+        private void EinstellungenOeffnen(object sender, RoutedEventArgs e)
         {
             Einstellungen einstellungen = new Einstellungen();
             einstellungen.ShowDialog();
+        }
+
+        private void KundenlisteFuellen()
+        {
+            Datenbank datenbank = new Datenbank();
+            string suche = "%" + kundenSuche.Text + "%";
+            string query = "SELECT Kundennummer, Vorname, Nachname, Straße, Ort FROM Kunden WHERE Kundennummer LIKE @suche OR Vorname LIKE @suche OR Nachname LIKE @suche OR Straße LIKE @suche OR Ort LIKE @suche";
+            SQLiteCommand sqliteCommand = new SQLiteCommand(query, datenbank.sqliteConnection);
+            sqliteCommand.Parameters.AddWithValue("@suche", suche);
+            datenbank.VerbindungOeffnen();
+            SQLiteDataAdapter sqliteDataAdapter = new SQLiteDataAdapter(sqliteCommand);
+            datenbank.VerbindungSchliessen();
+            DataTable dataTable = new DataTable("Kunden");
+            sqliteDataAdapter.Fill(dataTable);
+            kundenListe.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void SucheEingeben(object sender, TextChangedEventArgs e)
+        {
+            KundenlisteFuellen();
         }
     }
 }
