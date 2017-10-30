4d07331ee7aa6e3c4a2f09206b5718ad26eaf744
Henahax <mail@henahax.de>
2017-10-30 18:43:17 +0100

    Hinweise Layout und Sichtbarkeit verbessert, Kundensuche verbessert, Rechnungsliste implementiert, Rechnung anlegen implementiert

--- Bachelorarbeit/Bachelorarbeit/MainWindow.xaml.cs
+++ Bachelorarbeit/Bachelorarbeit/MainWindow.xaml.cs
@@ -16,17 +16,20 @@ namespace Bachelorarbeit
     {
         mainEntities _entities;
         CollectionViewSource _kundenViewSource;
+        CollectionViewSource _rechnungenViewSource;
         String kundenFilter = "";
         bool neuerKundeWirdAngelegt = false;
+        bool neueRechnungWirdAngelegt = false;
 
         public MainWindow()
         {
-            //TODO: Titel für Kunden
             //TODO: Datenbank relativer Pfad
+            //TODO: Suche Vorschauanzeige
 
             //TODO: Bei Datenbankänderungen, Identify Spalte neu konfigurieren im Model
             InitializeComponent();
             _kundenViewSource = ((CollectionViewSource)(this.FindResource("kundenViewSource")));
+            _rechnungenViewSource = ((CollectionViewSource)(this.FindResource("rechnungenViewSource")));
             Refresh();
         }
 
@@ -40,6 +43,8 @@ namespace Bachelorarbeit
 
             _entities.kunden.Load();
             _kundenViewSource.Source = _entities.kunden.Local;
+            _entities.rechnungen.Load();
+            _rechnungenViewSource.Source = _entities.rechnungen.Local;
         }
         
 
@@ -49,10 +54,11 @@ namespace Bachelorarbeit
             einstellungen.ShowDialog();
         }
 
-        private void SucheEingeben(object sender, TextChangedEventArgs e)
+        private void KundenSucheEingeben(object sender, TextChangedEventArgs e)
         {
             kundenFilter = kundenSuche.Text;
             Refresh();
+            kundenListe.SelectedItem = null;
         }
 
 
@@ -62,6 +68,9 @@ namespace Bachelorarbeit
 
             groupBoxKunde.IsEnabled = true;
             groupBoxKunden.IsEnabled = false;
+
+            tabRechnungen.IsEnabled = false;
+            tabAngebote.IsEnabled = false;
 
             kundeKundennummer.Content = kunde.kundennummer;
             kundeAnrede.Text = kunde.anrede;
@@ -92,7 +101,6 @@ namespace Bachelorarbeit
             MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Sind sie sicher, dass sie diesen Kunden löschen wollen?", "Kunden Löschen Bestätigung", System.Windows.MessageBoxButton.YesNo);
             if (messageBoxResult == MessageBoxResult.Yes)
             {
-
                 _entities.kunden.Remove(kunde);
                 _entities.SaveChanges();
                 Refresh();
@@ -124,6 +132,25 @@ namespace Bachelorarbeit
         {
             if (neuerKundeWirdAngelegt == true)
             {
+                if (kundeAnrede.SelectedItem == null)
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Anrede muss ausgefüllt sein!");
+                    return;
+                }
+                if (kundeAnrede.Text == "Herr" || kundeAnrede.Text == "Frau")
+                {
+                    if (kundeNachname.Text == "")
+                    {
+                        MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Nachname ausgefüllt sein!");
+                        return;
+                    }
+                }
+                if (kundeAnrede.Text == "Firma" && kundeFirma.Text == "")
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Firma muss ausgefüllt sein!");
+                    return;
+                }
+
                 kunden kunde = new kunden();
 
                 long kundennummer;
@@ -146,31 +173,15 @@ namespace Bachelorarbeit
                 kunde.webseite = kundeWebseite.Text;
                 kunde.notizen = kundeNotizen.Text;
 
-                if (kundeAnrede.SelectedItem == null)
-                {
-                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Anrede muss ausgefüllt sein!");
-                    return;
-                }
-                if (kundeAnrede.Text == "Herr" || kundeAnrede.Text == "Frau")
-                {
-                    if(kundeNachname.Text == "")
-                    {
-                        MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Nachname ausgefüllt sein!");
-                        return;
-                    }
-                }
-                if (kundeAnrede.Text == "Firma" && kundeFirma.Text == "")
-                {
-                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Firma muss ausgefüllt sein!");
-                    return;
-                }
-
                 _entities.kunden.Add(kunde);
                 _entities.SaveChanges();
                 Refresh();
 
                 groupBoxKunde.IsEnabled = false;
                 groupBoxKunden.IsEnabled = true;
+
+                tabRechnungen.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
 
                 kundeKundennummer.Content = null;
                 kundeAnrede.Text = null;
@@ -196,6 +207,17 @@ namespace Bachelorarbeit
             }
             else
             {
+                if (kundeAnrede.SelectedItem == null)
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Anrede muss ausgefüllt sein!");
+                    return;
+                }
+                if (kundeNachname.Text == "" && kundeFirma.Text == "")
+                {
+                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Nachname oder Firma muss ausgefüllt sein!");
+                    return;
+                }
+
                 kunden kunde = (kunden)kundenListe.SelectedItem;
 
                 kunde.anrede = kundeAnrede.Text;
@@ -214,17 +236,6 @@ namespace Bachelorarbeit
                 kunde.webseite = kundeWebseite.Text;
                 kunde.notizen = kundeNotizen.Text;
 
-                if (kundeAnrede.SelectedItem == null)
-                {
-                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Anrede muss ausgefüllt sein!");
-                    return;
-                }
-                if (kundeNachname.Text == "" && kundeFirma.Text == "")
-                {
-                    MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Nachname oder Firma muss ausgefüllt sein!");
-                    return;
-                }
-
                 _entities.kunden.Attach(kunde);
                 _entities.Entry(kunde).State = EntityState.Modified;
                 _entities.SaveChanges();
@@ -232,6 +243,9 @@ namespace Bachelorarbeit
 
                 groupBoxKunde.IsEnabled = false;
                 groupBoxKunden.IsEnabled = true;
+
+                tabRechnungen.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
 
                 kundeKundennummer.Content = null;
                 kundeAnrede.Text = null;
@@ -265,6 +279,9 @@ namespace Bachelorarbeit
                 groupBoxKunde.IsEnabled = false;
                 groupBoxKunden.IsEnabled = true;
 
+                tabRechnungen.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
+
                 kundeKundennummer.Content = null;
                 kundeAnrede.Text = null;
                 kundeTitel.Text = null;
@@ -293,6 +310,9 @@ namespace Bachelorarbeit
         {
             groupBoxKunde.IsEnabled = true;
             groupBoxKunden.IsEnabled = false;
+
+            tabRechnungen.IsEnabled = false;
+            tabAngebote.IsEnabled = false;
 
             kundeKundennummer.Content = null;
             kundeAnrede.Text = null;
@@ -329,7 +349,6 @@ namespace Bachelorarbeit
             kundenlisteBearbeiten.IsEnabled = false;
             kundenlisteLoeschen.IsEnabled = false;
             neuerKundeWirdAngelegt = true;
-
         }
 
         private void NeueZeile(object sender, RoutedEventArgs e)
@@ -341,26 +360,15 @@ namespace Bachelorarbeit
             */
         }
 
-        private void RechnungAbbrechen(object sender, RoutedEventArgs e)
-        {
-            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Sind sie sicher, dass sie den Bearbeitungsvorgang abbrechen wollen?\nAlle Änderungen gehen verloren.", "Abbrechen Bestätigung", System.Windows.MessageBoxButton.YesNo);
-            if (messageBoxResult == MessageBoxResult.Yes)
-            {
-                /*
-                //TODO: Rechnung deaktivieren
-                rechnungPositionenListe.Clear();
-                rechnungPositionen.ItemsSource = null;
-                rechnungPositionen.ItemsSource = rechnungPositionenListe;
-                */
-            }
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
             System.Windows.Data.CollectionViewSource kundenViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("kundenViewSource")));
-            // Laden Sie Daten durch Festlegen der CollectionViewSource.Source-Eigenschaft:
-            // kundenViewSource.Source = [generische Datenquelle]
+            // Load data by setting the CollectionViewSource.Source property:
+            // kundenViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource rechnungenViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("rechnungenViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // rechnungenViewSource.Source = [generic data source]
         }
 
         private void KundeAusgewaehlt(object sender, SelectedCellsChangedEventArgs e)
@@ -400,7 +408,7 @@ namespace Bachelorarbeit
             }
         }
 
-        private void Filter(object sender, FilterEventArgs e)
+        private void KundenFilter(object sender, FilterEventArgs e)
         {
             kunden kunde = e.Item as kunden;
             if (kunde.titel.ToLower().Contains(kundenFilter.ToLower()) || kunde.vorname.ToLower().Contains(kundenFilter.ToLower()) || kunde.nachname.ToLower().Contains(kundenFilter.ToLower()) || kunde.firma.ToLower().Contains(kundenFilter.ToLower()) || kunde.strasse.ToLower().Contains(kundenFilter.ToLower()) || kunde.postleitzahl.ToLower().Contains(kundenFilter.ToLower()) || kunde.ort.ToLower().Contains(kundenFilter.ToLower()) || kunde.land.ToLower().Contains(kundenFilter.ToLower()) || kunde.telefon.ToLower().Contains(kundenFilter.ToLower()) || kunde.telefax.ToLower().Contains(kundenFilter.ToLower()) || kunde.mobiltelefon.ToLower().Contains(kundenFilter.ToLower()) || kunde.email.ToLower().Contains(kundenFilter.ToLower()) || kunde.webseite.ToLower().Contains(kundenFilter.ToLower()) || kunde.notizen.ToLower().Contains(kundenFilter.ToLower()))
@@ -410,6 +418,201 @@ namespace Bachelorarbeit
             {
                 e.Accepted = false;
             }
+        }
+
+        private void RechnungslisteAnlegen(object sender, RoutedEventArgs e)
+        {
+            groupBoxRechnung.IsEnabled = true;
+            groupBoxRechnungen.IsEnabled = false;
+
+            tabKunden.IsEnabled = false;
+            tabAngebote.IsEnabled = false;
+
+            rechnungRechnungsnummer.Content = null;
+            //TODO:
+
+            long rechnungsnummer;
+
+            if (_entities.rechnungen.Any() == false)
+            {
+                rechnungsnummer = 1;
+            }
+            else
+            {
+                long hoechsteRechnungsnummer = _entities.rechnungen.Max(r => r.rechnungsnummer);
+                rechnungsnummer = ++hoechsteRechnungsnummer;
+            }
+
+            rechnungRechnungsnummer.Content = rechnungsnummer;
+
+            rechnungsListe.SelectedItem = null;
+            rechnungslisteBearbeiten.IsEnabled = false;
+            rechnungslisteLoeschen.IsEnabled = false;
+            neueRechnungWirdAngelegt = true;
+        }
+
+        private void RechnungslisteBearbeiten(object sender, RoutedEventArgs e)
+        {
+            rechnungen rechnung = (rechnungen)rechnungsListe.SelectedItem;
+
+            groupBoxRechnung.IsEnabled = true;
+            groupBoxRechnungen.IsEnabled = false;
+
+            tabKunden.IsEnabled = false;
+            tabAngebote.IsEnabled = false;
+
+
+            rechnungRechnungsnummer.Content = rechnung.rechnungsnummer;
+            //TODO:
+        }
+
+        private void RechnungslisteLoeschen(object sender, RoutedEventArgs e)
+        {
+            rechnungen rechnung = (rechnungen)rechnungsListe.SelectedItem;
+
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Sind sie sicher, dass sie diesen Kunden löschen wollen?", "Kunden Löschen Bestätigung", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+
+                _entities.rechnungen.Remove(rechnung);
+                //TODO: Prüfen ob Positionen gelöscht werden
+                _entities.SaveChanges();
+                Refresh();
+
+                rechnungRechnungsnummer.Content = null;
+                //TODO:
+
+                rechnungsListe.SelectedItem = null;
+                rechnungslisteBearbeiten.IsEnabled = false;
+                rechnungslisteLoeschen.IsEnabled = false;
+            }
+        }
+
+        private void RechnungSpeichern(object sender, RoutedEventArgs e)
+        {
+            if (neueRechnungWirdAngelegt == true)
+            {
+                //TODO: Pflichfelder prüfen
+
+                long rechnungsnummer;
+                Int64.TryParse(kundeKundennummer.Content.ToString(), out rechnungsnummer);
+                
+                long kundennummer;
+                Int64.TryParse(rechnungKundennummer.Content.ToString(), out kundennummer);
+
+                var query = from kunden in _entities.kunden where kunden.kundennummer == kundennummer select kunden;
+                
+                //TODO: evtl unsauber
+                long kundenId = 0;
+                foreach (var result in query)
+                {
+                    kundenId = result.id;
+                }
+
+                rechnungen rechnung = new rechnungen();
+
+                rechnung.rechnungsnummer = rechnungsnummer;
+                rechnung.kunde_id = kundenId;
+                //TODO
+
+                _entities.rechnungen.Add(rechnung);
+                _entities.SaveChanges();
+                Refresh();
+
+                groupBoxRechnung.IsEnabled = false;
+                groupBoxRechnungen.IsEnabled = true;
+
+                tabKunden.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
+
+                rechnungRechnungsnummer.Content = null;
+                rechnungKundennummer.Content = null;
+                //TODO
+
+                rechnungsListe.SelectedItem = null;
+                rechnungslisteBearbeiten.IsEnabled = false;
+                rechnungslisteLoeschen.IsEnabled = false;
+                neueRechnungWirdAngelegt = false;
+            }
+            else
+            {
+                //TODO: Pflichfelder prüfen
+
+                long kundennummer;
+                Int64.TryParse(rechnungKundennummer.Content.ToString(), out kundennummer);
+
+                var query = from kunden in _entities.kunden where kunden.kundennummer == kundennummer select kunden;
+
+                //TODO: evtl unsauber
+                long kundenId = 0;
+                foreach (var result in query)
+                {
+                    kundenId = result.id;
+                }
+
+                rechnungen rechnung = (rechnungen)rechnungsListe.SelectedItem;
+
+                rechnung.kunde_id = kundenId;
+                //TODO
+
+                _entities.rechnungen.Attach(rechnung);
+                _entities.Entry(rechnung).State = EntityState.Modified;
+                _entities.SaveChanges();
+                Refresh();
+
+                groupBoxRechnung.IsEnabled = false;
+                groupBoxRechnungen.IsEnabled = true;
+
+                tabKunden.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
+
+                rechnungRechnungsnummer.Content = null;
+                rechnungKundennummer.Content = null;
+                //TODO
+
+                rechnungsListe.SelectedItem = null;
+                rechnungslisteBearbeiten.IsEnabled = false;
+                rechnungslisteLoeschen.IsEnabled = false;
+                neueRechnungWirdAngelegt = false;
+            }
+        }
+
+        private void RechnungSpeichernPDF(object sender, RoutedEventArgs e)
+        {
+            //TODO:
+        }
+
+        private void RechnungDrucken(object sender, RoutedEventArgs e)
+        {
+            //TODO:
+            PrintDialog druckdialog = new PrintDialog();
+            druckdialog.ShowDialog();
+        }
+
+        private void RechnungAbbrechen(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Sind sie sicher, dass sie den Bearbeitungsvorgang abbrechen wollen?\nAlle Änderungen gehen verloren.", "Abbrechen Bestätigung", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                groupBoxRechnung.IsEnabled = false;
+                groupBoxRechnungen.IsEnabled = true;
+
+                tabKunden.IsEnabled = true;
+                tabAngebote.IsEnabled = true;
+
+                rechnungRechnungsnummer.Content = null;
+                //TODO:
+
+                rechnungsListe.SelectedItem = null;
+                rechnungslisteBearbeiten.IsEnabled = false;
+                rechnungslisteLoeschen.IsEnabled = false;
+                neueRechnungWirdAngelegt = false;
+            }
+        }
+
+        private void RechnungAusgewaehlt(object sender, SelectedCellsChangedEventArgs e)
+        {
+
         }
     }
 }
